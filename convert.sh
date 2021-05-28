0271368380bed640b30f6780834fb39644cfcdc8
thopDB <65649957+thopDB@users.noreply.github.com>
2021-05-28 18:07:18 +0530

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,7 +1,7 @@
 # Change input and output details
 
 output_path="11db" # Output Path (Same will be pushed in main also)
-input_url="https://s42.ibomma.download/Ek-Mini-Katha-2021.mp4" # Input direct file url
+input_url="https://ahatvmp4ns.akamaized.net/aha-tv/Renditions/20210526/ANUKONI_ATHIDHI_For_Upload_26th/ANUKONI_ATHIDHI_For_Upload_26th_270.mp4?__token__=exp=1622224104~acl=/aha-tv/Renditions/20210526/ANUKONI_ATHIDHI_For_Upload_26th/*~hmac=c5aebaa42bdb203f354080c005347e2def8fdfd02c42c3c58fb016ddba820be6" # Input direct file url
 input_extension="mp4" # Extension of file url
 
 
