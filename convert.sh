7ac9dbd1d2179e20d27fe84ec560aeb1b831d0e7
thopDB <65649957+thopDB@users.noreply.github.com>
2021-09-17 22:27:35 +0530

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,7 +1,7 @@
 # Change input and output details
 
 output_path="db6" # Output Path (Same will be pushed in main also)
-input_url="https://botxdirect.herokuapp.com/153708896946734446151767440/Development_up_to_2nd_week_1.mp4" # Input direct file url
+input_url="https://botxdirect.herokuapp.com/154090228040226170002773392/Duw2.mp4" # Input direct file url
 input_extension="mp4" # Extension of file url
 
 
