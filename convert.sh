2964e905f09c612d151ef2b2591b3e9454a32541
thopDB <65649957+thopDB@users.noreply.github.com>
2021-11-02 12:17:05 +0530

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,8 +1,8 @@
 # Change input and output details
 
-output_path="db7" # Output Path (Same will be pushed in main also)
-input_url="https://botxdirect.herokuapp.com/154090228040226170002773392/Duw2.mp4" # Input direct file url
-input_extension="mp4" # Extension of file url
+output_path="jb" # Output Path (Same will be pushed in main also)
+input_url="https://uploadbot2.tk/dl/1038619777/www.7MovieRulz.lv_JAI_BHIM_2021_720p_Telugu_Org_Vers_HDRip_HEVC_AAC_950MB_ESub.mkv" # Input direct file url
+input_extension="mkv" # Extension of file url
 
 
 
@@ -12,12 +12,12 @@ wget --quiet -O video.$input_extension $input_url
 mkdir $output_path
 
 ffmpeg -hide_banner -y -i video.$input_extension \
-  -vf scale=w=640:h=360:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod  -b:v 800k -maxrate 856k -bufsize 1200k -b:a 96k -hls_segment_filename $output_path/360p_%03d.ts $output_path/360p.m3u8 
+  -vf scale=w=842:h=480:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 1400k -maxrate 1498k -bufsize 2100k -b:a 128k -hls_segment_filename $output_path/480p_%03d.ts $output_path/480p.m3u8 
 
 rm video.$input_extension
 cd $output_path
 
 echo '#EXTM3U
 #EXT-X-VERSION:3
-#EXT-X-STREAM-INF:BANDWIDTH=800000,RESOLUTION=640x360
-360p.m3u8' > master.m3u8
+#EXT-X-STREAM-INF:BANDWIDTH=1400000,RESOLUTION=842x480
+480p.m3u8' > master.m3u8
